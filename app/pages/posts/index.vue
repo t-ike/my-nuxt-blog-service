0d378ef8435accd156bc65297a225923d39480c4
t-ikematsu <taishi.ikematsu@photosynth.co.jp>
2019-12-24 00:08:36 +0900

    投稿一覧ページ追加

---
+++ app/pages/posts/index.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+    section.container
+        v-card(width="100%")
+            v-card-title 新着投稿
+            v-divider
+            v-data-table(
+                :headers="headers"
+                :items="posts"
+                class="elevation-1"
+            )
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'タイトル',
+          align: 'left',
+          value: 'title',
+          width: '60%'
+        },
+        {
+          text: '投稿者',
+          value: 'id'
+        },
+        {
+          text: '投稿日時',
+          value: 'created_at'
+        }
+      ],
+      posts: [
+        {
+          id: '001',
+          title: 'Title1',
+          body: 'aa ii uu',
+          created_at: '2018/12/22 12:00:00'
+        },
+        {
+          id: '002',
+          title: 'Title2',
+          body: 'aa ii uu',
+          created_at: '2019/01/22 13:00:00'
+        }
+      ]
+    }
+  }
+}
+</script>
