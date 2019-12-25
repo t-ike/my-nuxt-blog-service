1f3f72bf007a36685727403ff339aed00b62fafb
t-ikematsu <taishi.ikematsu@photosynth.co.jp>
2019-12-25 17:21:26 +0900

    vuetify snackbarテスト一時コミット

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,13 +16,16 @@
           v-checkbox(v-model="isCreateMode" label="アカウントを作成する").py-0.my-0
           v-spacer
           v-card-actions
+            //- v-btn(color="primary" @click="snackbar=true")
             v-btn(color="primary" @click="handleClickSubmit")
               span.px-3 {{buttonText}}
+            v-snackbar(v-model="this.snackbar.status")
+              | {{ this.snackbar.message }}
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-// import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import Cookies from 'universal-cookie'
 
 export default {
   asyncData({ redirect, store }) {
@@ -39,22 +42,20 @@ export default {
   computed: {
     buttonText() {
       return this.isCreateMode ? '新規登録' : 'ログイン'
-    }
+    },
+    // storeのuserのgetterを展開
+    ...mapGetters(['user', 'snackbar'])
   },
   methods: {
     async handleClickSubmit() {
+      const cookies = new Cookies()
       if (this.isCreateMode) {
         try {
           await this.register({ ...this.formData })
-          this.$notify({
-            type: 'success',
-            title: 'アカウント作成完了',
-            message: `${this.formData.id} として登録しました`,
-            position: 'bottom-right',
-            duration: 1000
-          })
+          cookies.set('user', JSON.stringify(this.user))
           this.$router.push('/posts/')
         } catch (e) {
+          console.log(e)
           this.$notify.error({
             title: 'アカウント作成失敗',
             message: '既に登録されているか、不正なユーザー ID です',
@@ -65,15 +66,10 @@ export default {
       } else {
         try {
           await this.login({ ...this.formData })
-          this.$notify({
-            type: 'success',
-            title: 'ログイン成功',
-            message: `${this.formData.id} としてログインしました`,
-            position: 'bottom-right',
-            duration: 1000
-          })
-          this.$router.push('/posts/')
+          cookies.set('user', JSON.stringify(this.user))
+          // this.$router.push('/posts/')
         } catch (e) {
+          console.log(e)
           this.$notify.error({
             title: 'ログイン失敗',
             message: '不正なユーザー ID です',
--- app/store/index.js
+++ app/store/index.js
@@ -1,32 +1,58 @@
 export const state = () => ({
   isLoggedIn: false,
-  user: null
+  user: null,
+  snackbar: {
+    status: false,
+    message: null
+  }
 })
 
 export const getters = {
   isLoggedIn: (state) => state.isLoggedIn,
-  user: (state) => state.user
+  user: (state) => state.user,
+  snackbar: (state) => state.snackbar
 }
 
 export const mutations = {
   setUser(state, { user }) {
     state.user = user
     state.isLoggedIn = true
+  },
+  setSnackBar(state, { snackbar }) {
+    state.snackbar = snackbar
   }
 }
 
 export const actions = {
-  async login({ commit }, { id }) {
-    const user = await this.$axios.$get(`/users/${id}.json`)
+  // async login({ commit }, { id }) {
+  login({ commit }, { id }) {
+    const user = {
+      id: 1
+    }
+    // const user = await this.$axios.$get(`/users/${id}.json`)
     if (!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
+    const snackbar = {
+      status: true,
+      message: 'Success'
+    }
+    commit('setSnackBar', { snackbar })
   },
-  async register({ commit }, { id }) {
+  // async register({ commit }, { id }) {
+  register({ commit }, { id }) {
     const payload = {}
     payload[id] = { id }
-    await this.$axios.$patch(`/users.json`, payload)
-    const user = await this.$axios.$get(`/users/${id}.json`)
+    // await this.$axios.$patch(`/users.json`, payload)
+    // const user = await this.$axios.$get(`/users/${id}.json`)
+    const user = {
+      id: 1
+    }
     if (!user.id) throw new Error('Invalid user')
     commit('setUser', { user })
+    const snackbar = {
+      status: true,
+      message: 'Success Register'
+    }
+    commit('setSnackBar', { snackbar })
   }
 }
